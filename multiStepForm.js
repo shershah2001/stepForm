42fde3c520f5b1f8836985870b743c91f5eb6d85
Md. Shershah <159061850+shershah2001@users.noreply.github.com>
2024-09-02 18:21:20 +0530

    step form

--- multiStepForm.js
+++ multiStepForm.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let currentStep=0;
   const updateProgress = ()=>{
     let width = currentStep/(steps.length-1);
+    // console.log("width ===>",steps.length-1)
         progress.style.transform = `scaleX(${width})`;
         stepsContainer.style.height = steps[currentStep].offsetHeight + "px";
         stepIndicators.forEach((indicator,index)=>{
@@ -21,6 +22,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         })
         
         steps.forEach((step,index)=>{
+          //  console.log("first-name===>",currentStep)
             step.style.transform=`translateX(-${currentStep*100}%)`
             step.classList.toggle('current',currentStep === index);
         })
@@ -42,10 +44,10 @@ document.addEventListener('DOMContentLoaded',()=>{
 const inputs = form.querySelectorAll('input,textarea');
 inputs.forEach(input=>input.addEventListener('focus',(e)=>{
     const focusedElement=e.target;
-    
+    // console.log("focusedElement===>",focusedElement)
     // get the step where the focused element belongs
     const focusedStep = [...steps].findIndex(step=>step.contains(focusedElement));
-
+    console.log("focusedStep===>",focusedStep)
     if(focusedStep !==-1 && focusedStep!==currentStep){
        if(!isValidStep()) return; 
 
@@ -60,9 +62,9 @@ form.addEventListener("submit", (e) => {
     e.preventDefault(); // prevent form submission
 
     // if (!form.checkValidity()) return;
-
+    
     const formData = new FormData(form);
-
+    // console.log("form ===>",formData)
     // send the data somewhere
     console.log(Object.fromEntries(formData));
 
@@ -88,6 +90,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
      if(!isValidStep()) return ;
      if(currentStep<steps.length-1){
+      
         currentStep++;
         updateProgress();
      }
